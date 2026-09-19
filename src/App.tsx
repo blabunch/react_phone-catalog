60643cc5771ea3e27229247ba4b9bb1350333df5
Bohdan Labunets <blabunch@gmail.com>
2026-09-19 19:23:30 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export const App = () => {
   return (
     <CartProvider>
       <FavoritesProvider>
-        <BrowserRouter basename="/react_phone-catalog">
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<HomePage />} />
